423e57bc5ddf197ec46ac76223cb487b05c8ef6a
ebrarilhan <salihaebrarilhan@gmail.com>
2022-12-21 22:02:03 +0100

    21-12-22

---
+++ src/test/java/day04_Maven/C01_MavenIlkTest.java
@@ -0,0 +1,56 @@
+package day04_Maven;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.time.Duration;
+import java.util.List;
+
+public class C01_MavenIlkTest {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        //1-https://www.amazon.com/ sayfasina gidelim
+        driver.get("https://www.amazon.com/");
+
+        //2-arama kutusunu locate edelim
+        WebElement aramaKutusu = driver.findElement(By.xpath("//*[@id='twotabsearchtextbox']"));
+
+        //3-"Samsung headphones" ile arama yapalim
+        aramaKutusu.sendKeys("Samsung headphones"); //, koyup Keys.ENTER ile de enter yapabiliriz.
+        aramaKutusu.submit(); // ama biz bu sefer boyle entera bastik.
+
+        //4-Bulunan sonuc sayisini yazdiralim
+        String sonucSayisi = driver.findElement(By.xpath("(//*[@class='sg-col-inner'])[1]")).getText();
+        System.out.println(sonucSayisi);
+
+        //5-Ilk urunu tiklayalim
+        driver.findElement(By.xpath("(//*[@class='s-image'])[1]")).click();
+
+        //6-Sayfadaki tum basliklari yazdiralim.
+        List<WebElement> sayfaBasliklariListesi = driver.findElements(By.xpath("//h1"));
+
+        for (WebElement w : sayfaBasliklariListesi){
+
+            System.out.println(w.getText());
+
+        }
+
+        //Lambda ile alma
+        sayfaBasliklariListesi.forEach(t -> System.out.println(t.getText()));
+
+        //sayfayi kapatiniz.
+        driver.close();
+
+    }
+
+}
